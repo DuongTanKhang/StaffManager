c8a9793aa6d2ee1e9853f51923671b648e81659f
Duong Tan Khang <barbiecute306@gmail.com>
2025-09-29 19:43:22 +0700

    Initial commit

---
+++ src/main/java/DBConnection/SqlServerConnection.java
@@ -0,0 +1,21 @@
+package DBConnection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SqlServerConnection {
+	public static void main(String[] args) {
+		var url = "jdbc:sqlserver://localhost:1433;databaseName=Staff_Timekeeping_Management;encrypt=true;trustServerCertificate=true;";
+		var user = "sa";
+		var password = "123";
+
+		try (var conn = DriverManager.getConnection(url, user, password)) {
+			if (conn != null) {
+				System.out.println("Kết nối SQL Server thành công!");
+			}
+		} catch (SQLException e) {
+			System.out.println("Lỗi kết nối: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
